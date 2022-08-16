767dc00a4dea5714e531b43c9978e97d8e2dd29b
harman singh <charmanksingh9765@gmail.com>
2022-08-16 14:30:04 +0530

    Crypto APP

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  CryptoCurrency,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />}></Route>
+              <Route exact path="/exchanges" element={<Exchanges />}></Route>
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<CryptoCurrency />}
+              ></Route>
+              <Route
+                exact
+                path="/crypto/:coindId"
+                element={<CryptoDetails />}
+              ></Route>
+              <Route exact path="/news" element={<News />}></Route>
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            CryptoApp
+            <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Key": "4d18bb4d53mshce01f87b603e0f6p1d0933jsn97f40ef3b4ce",
+  "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+
+
